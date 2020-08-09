58150048ab11103d6c70ec7ee0d9524a384996db
Beroni <beronimagalhaes@gmail.com>
2020-08-09 19:32:36 -0300

    Setup Storybook with Typescript and Knobs Addon

---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,16 @@
+import Main from '.'
+
+import { withKnobs, text } from '@storybook/addon-knobs'
+
+export default {
+	title: 'Main',
+	component: Main,
+	decorators: [withKnobs]
+}
+
+export const Basic = () => (
+	<Main
+		title={text('Title', 'React Avançado')}
+		description={text('Description', 'TypeScript, NextJS e Styled Components')}
+	/>
+)
